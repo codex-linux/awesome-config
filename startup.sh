bfd64aeac469af6e8d148bc0002457d93d88200c
codex-linux <145232542+codex-linux@users.noreply.github.com>
2023-09-16 22:50:55 +0100

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xrandr --output DisplayPort-1 --mode 1920x1080 --rate 75.40
+xrandr --output DisplayPort-0 --mode 1920x1080 --rate 74.97
+xrandr --output DisplayPort-1 --set TearFree on
