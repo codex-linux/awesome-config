de5bc182bf515b103a7fc3c39aa4ea52ca75133f
codex-linux <145232542+codex-linux@users.noreply.github.com>
2023-12-12 19:15:57 +0000

    Add files via upload

--- startup.sh
+++ startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-xrandr --output DisplayPort-1 --mode 1920x1080 --rate 75.40
-xrandr --output DisplayPort-0 --mode 1920x1080 --rate 74.97
-xrandr --output DisplayPort-1 --set TearFree on
+
+xrandr --output eDP --off --output DisplayPort-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 0x0 --rotate normal
+
+
+xrandr --output DisplayPort-1 --mode 1920x1080 --rate 74.97
+xrandr --output DisplayPort-0 --mode 1920x1080 --rate 144.04
+xrandr --output DisplayPort-0 --set TearFree on
